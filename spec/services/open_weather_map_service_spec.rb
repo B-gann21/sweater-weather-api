6ce7687a1a0b2e22ead8da15ed831017d910c97a
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-13 14:30:10 -0600

    Test: OpenWeatherMapService has daily_weather in response

--- spec/services/open_weather_map_service_spec.rb
+++ spec/services/open_weather_map_service_spec.rb
@@ -57,7 +57,100 @@ RSpec.describe OpenWeatherMapService do
         expect(@result[:current][:weather][0][:icon]).to be_a String
       end
 
-      it 'has daily weather'
+      it 'has daily weather' do
+        expect(@result).to have_key :daily
+        expect(@result[:daily]).to be_an Array
+
+        @result[:daily].each do |forecast|
+          expect(forecast).to have_key :dt
+          expect(forecast[:dt]).to be_an Integer
+
+          expect(forecast).to have_key :sunset
+          expect(forecast[:sunrise]).to be_an Integer
+
+          expect(forecast).to have_key :sunset
+          expect(forecast[:sunset]).to be_an Integer
+
+          expect(forecast).to have_key :moonrise
+          expect(forecast[:moonrise]).to be_an Integer
+
+          expect(forecast).to have_key :moonset
+          expect(forecast[:moonset]).to be_an Integer
+
+          expect(forecast).to have_key :moon_phase
+          expect(forecast[:moon_phase]).to be_a Float
+
+          expect(forecast).to have_key :temp
+          expect(forecast[:temp]).to be_a Hash
+
+          expect(forecast[:temp]).to have_key :day
+          expect(forecast[:temp][:day]).to be_a Float
+
+          expect(forecast[:temp]).to have_key :min
+          expect(forecast[:temp][:min]).to be_a Float
+
+          expect(forecast[:temp]).to have_key :max
+          expect(forecast[:temp][:max]).to be_a Float
+
+          expect(forecast[:temp]).to have_key :night
+          expect(forecast[:temp][:night]).to be_a Float
+
+          expect(forecast[:temp]).to have_key :eve
+          expect(forecast[:temp][:eve]).to be_a Float
+
+          expect(forecast[:temp]).to have_key :morn
+          expect(forecast[:temp][:morn]).to be_a Float
+
+          expect(forecast).to have_key :feels_like
+          expect(forecast[:feels_like]).to be_a Hash
+
+          expect(forecast[:feels_like]).to have_key :day
+          expect(forecast[:feels_like][:day]).to be_a(Float).or be_a(Integer)
+
+          expect(forecast[:feels_like]).to have_key :night
+          expect(forecast[:feels_like][:night]).to be_a(Float).or be_a(Integer)
+          
+          expect(forecast[:feels_like]).to have_key :eve
+          expect(forecast[:feels_like][:eve]).to be_a(Float).or be_a(Integer)
+
+          expect(forecast[:feels_like]).to have_key :morn
+          expect(forecast[:feels_like][:morn]).to be_a(Float).or be_a(Integer)
+
+          expect(forecast).to have_key :pressure
+          expect(forecast[:pressure]).to be_an Integer
+          
+          expect(forecast).to have_key :humidity
+          expect(forecast[:humidity]).to be_an Integer
+          
+          expect(forecast).to have_key :dew_point
+          expect(forecast[:dew_point]).to be_a Float
+          
+          expect(forecast).to have_key :wind_speed
+          expect(forecast[:wind_speed]).to be_a Float
+          
+          expect(forecast).to have_key :wind_deg
+          expect(forecast[:wind_deg]).to be_an Integer
+          
+          expect(forecast).to have_key :wind_gust
+          expect(forecast[:wind_gust]).to be_a Float
+          
+          expect(forecast).to have_key :weather
+          expect(forecast[:weather]).to be_an Array
+          expect(forecast[:weather]).to be_all Hash
+
+          expect(forecast[:weather][0]).to have_key :id
+          expect(forecast[:weather][0][:id]).to be_an Integer
+
+          expect(forecast[:weather][0]).to have_key :main
+          expect(forecast[:weather][0][:main]).to be_a String
+
+          expect(forecast[:weather][0]).to have_key :description
+          expect(forecast[:weather][0][:description]).to be_a String
+
+          expect(forecast[:weather][0]).to have_key :icon
+          expect(forecast[:weather][0][:icon]).to be_a String
+        end
+      end
 
       it 'has hourly weather' do
         expect(@result).to have_key :hourly
