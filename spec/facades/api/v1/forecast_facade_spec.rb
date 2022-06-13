20bb1d1067c498c89647ca40cd0807507118ec33
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-12 19:08:46 -0600

    Test: ForecastFacade.hourly_forecast returns Forecast objects

--- spec/facades/api/v1/forecast_facade_spec.rb
+++ spec/facades/api/v1/forecast_facade_spec.rb
@@ -2,14 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ForecastFacade do
   context 'class methods' do
+    it '.hourly_forecast returns an array of Forecast objects' do
+      forecasts = ForecastFacade.hourly_forecast('denver,co')
+
+      expect(forecasts).to be_an Array
+      expect(forecasts).to be_all Forecast
+    end
+
     it '.current_forecast returns a CurrentForecast object' do
-      forecast = ForecastFacade.current_forecast('Denver')
+      forecast = ForecastFacade.current_forecast('denver,co')
 
       expect(forecast).to be_a CurrentForecast
     end
 
     it '.daily_forecast returns an array of DailyForecast objects'
-    it '.hourly_forecast returns an array of HourlyForecast objects'
     it '.full_forecast returns a ForecastRepo object'
   end
 end
