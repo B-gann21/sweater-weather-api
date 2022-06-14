c233246555ae99d3d2e1cb3098cdae9110d62d86
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-14 10:12:01 -0600

    Refactor: forgot to actually '.save' users upon successful validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,9 @@ class Api::V1::UsersController < ApplicationController
     user = User.build_from_request(request.body.read)
 
     if user.valid_and_confirmed_password?
+      user.save
       render json: Api::V1::UserSerializer.user_show(user), status: 201
+
     else
       render json: Api::V1::UserErrorSerializer.bad_create_request(user), status: 400
     end
--- app/serializers/api/v1/user_error_serializer.rb
+++ app/serializers/api/v1/user_error_serializer.rb
@@ -1,7 +1,8 @@
 class Api::V1::UserErrorSerializer
   def self.bad_create_request(user)
-    if user.email.empty? || User.find_by(email: user.email)
+    if (user.email.nil? || user.email.empty?) || User.find_by(email: user.email)
       { error: 'invalid email' }
+  
     elsif user.password != user.password_confirmation
       { error: 'passwords must match' }
     end
