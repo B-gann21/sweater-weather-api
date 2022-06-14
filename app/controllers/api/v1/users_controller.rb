89c647d61f7a04368ec9e76c9fc4cad7d617b420
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-14 09:30:00 -0600

    Feat: User.build_from_request and user.valid_and_confirmed_password? passing tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::UsersController < ApplicationController
   before_action :validate_headers
 
   def create
-    body = JSON.parse(request.body.read, symbolize_names: true)
-    user = User.new(body)
+    user = User.build_from_request(request.body.read)
 
-    if user.save
+    if user.valid_and_confirmed_password?
       render json: Api::V1::UserSerializer.user_show(user)
     else
       render json: Api::V1::UserErrorSerializer.bad_request(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
 
   validates_presence_of :api_key
+
+  def self.build_from_request(body)
+    json = JSON.parse(body, symbolize_names: true) 
+    user = User.new(json)
+    user.api_key = SecureRandom.urlsafe_base64
+    user
+  end
+
+  def valid_and_confirmed_password?
+    valid? && (password == password_confirmation)
+  end
 end
