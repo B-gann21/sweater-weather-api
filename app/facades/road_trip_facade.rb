4ae780d7274123873e7a41e65ba337277d5fa56a
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-14 15:06:21 -0600

    WIP: RoadTripFacade set up to initialize POROs

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -4,8 +4,11 @@ class RoadTripFacade < ForecastFacade
     weather_at_arrival = hourly_forecast(end_city)[travel_time.to_i - 1]
     
     trip_hash = {}
+    trip_hash[:start_city] = start_city
+    trip_hash[:end_city] = end_city
     trip_hash[:travel_time] = travel_time
     trip_hash[:weather] = weather_at_arrival
+
     RoadTrip.new(trip_hash)
   end
 end
