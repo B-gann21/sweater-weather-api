019c3d039acfe995e7fe6f26341cbc2d4d4922cc
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-14 17:37:01 -0600

    Refactor: use low level caching in the RoadTrip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,16 +1,18 @@
 class RoadTripFacade < ForecastFacade
   def self.build_road_trip(start_city, end_city)
-    trip_hash = {}
-    trip_hash[:start_city] = start_city
-    trip_hash[:end_city] = end_city
+    Rails.cache.fetch("#{start_city} to #{end_city}", expires_in: 1.hour) do
+      trip_hash = {}
+      trip_hash[:start_city] = start_city
+      trip_hash[:end_city] = end_city
 
-    if time = check_travel_time(start_city, end_city)
-      trip_hash[:travel_time] = time
-      trip_hash[:weather] = weather_at_arrival(end_city, time.to_i - 1)
-    else
-      trip_hash[:travel_time] = 'impossible'
+      if time = check_travel_time(start_city, end_city)
+        trip_hash[:travel_time] = time
+        trip_hash[:weather] = weather_at_arrival(end_city, time.to_i - 1)
+      else
+        trip_hash[:travel_time] = 'impossible'
+      end
+      RoadTrip.new(trip_hash)
     end
-    RoadTrip.new(trip_hash)
   end
 
   def self.weather_at_arrival(end_city, hour_index)
