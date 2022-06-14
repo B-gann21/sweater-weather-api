1b7f9590d1433495b752eacabc0b24e5533e8aa9
Brylan Gannon <brylan.gannon112@gmail.com>
2022-06-14 16:25:51 -0600

    Refactor: extract  variable in the RoadTripFacade to a helper method

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,14 +1,17 @@
 class RoadTripFacade < ForecastFacade
   def self.build_road_trip(start_city, end_city)
     travel_time = MapQuestService.get_directions(start_city, end_city)[:route][:formattedTime]
-    weather_at_arrival = hourly_forecast(end_city)[travel_time.to_i - 1]
     
     trip_hash = {}
     trip_hash[:start_city] = start_city
     trip_hash[:end_city] = end_city
     trip_hash[:travel_time] = travel_time
-    trip_hash[:weather] = weather_at_arrival
+    trip_hash[:weather] = weather_at_arrival(end_city, travel_time.to_i - 1)
 
     RoadTrip.new(trip_hash)
+  end
+
+  def self.weather_at_arrival(end_city, hour_index)
+    hourly_forecast(end_city)[hour_index]
   end
 end
